55ecd4cc54f43f1bf96fa1cbae13d291f5078af3
Yanethortiz <yopg432@gmail.com>
2026-09-24 13:00:39 -0600

    fix: colocar Analytics en head para Search Console

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,9 +46,9 @@ export default function RootLayout({
         <Analytics />
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-analytics" strategy="beforeInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
